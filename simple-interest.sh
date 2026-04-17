fd71e0b6e192560575d7a0ea26592005e681e34b
punit-0522 <punitjaiswal785621@gmail.com>
2026-04-17 14:17:57 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+echo "Simple Interest Calculator"
+
 echo "Enter Principal:"
 read p
 
 echo "Enter Rate of Interest:"
 read r
 
-echo "Enter Time:"
+echo "Enter Time Period:"
 read t
 
-si=$(echo "scale=2; $p * $r * $t / 100" | bc)
+si=$((p * r * t / 100))
 
 echo "Simple Interest is: $si"
